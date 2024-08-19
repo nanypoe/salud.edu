5824d8abf769fcc3b7f7fc4c4ca484bd36ddb01f
nanypoe <162480838+nanypoe@users.noreply.github.com>
2024-08-19 16:45:03 -0600

    agregando base de datos
    bd

---
+++ database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE saludedu;
+USE saludedu;
+
+-- Tabla ESCUELAS
+CREATE TABLE Escuelas (
+    id_escuela INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255),
+    telefono VARCHAR(50),
+    email VARCHAR(100),
+    longitud DOUBLE,
+    latitud DOUBLE
+);
+
+-- Tabla MAESTROS
+CREATE TABLE maestros (
+    id_maestros INT AUTO_INCREMENT PRIMARY KEY,
+    id_escuela INT,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    email VARCHAR(100),
+    telefono VARCHAR(50),
+    perfil VARCHAR(100),
+    FOREIGN KEY (id_escuela) REFERENCES Escuelas(id_escuela) ON DELETE CASCADE
+);
