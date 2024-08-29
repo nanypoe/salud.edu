03f2dba4685ba730abafa4c03d0f9a5f774f625a
nanypoe <162480838+nanypoe@users.noreply.github.com>
2024-08-29 15:47:17 -0600

    Actualizando Base de Datos
    Se ha actualizado la Base de Datos para integrar la funcionalidad Perfiles de Salud Estudiantil

--- database.sql
+++ database.sql
@@ -2,40 +2,82 @@ CREATE DATABASE saludedu;
 USE saludedu;
 
 -- Tabla ESCUELAS
-CREATE TABLE Escuelas (
+CREATE TABLE escuelas (
     id_escuela INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     direccion VARCHAR(255),
     telefono VARCHAR(50),
     longitud DOUBLE,
     latitud DOUBLE
-);
+)ENGINE=InnoDB;
 
--- Tabla MAESTROS
-CREATE TABLE maestros (
-    id_maestros INT AUTO_INCREMENT PRIMARY KEY,
+-- Tabla Estudiantes
+CREATE table estudiantes (
+	id_estudiante INT PRIMARY KEY AUTO_INCREMENT,
     id_escuela INT,
-    nombre VARCHAR(255) NOT NULL,
-    apellido VARCHAR(255) NOT NULL,
-    email VARCHAR(100),
-    telefono VARCHAR(50),
-    perfil VARCHAR(100),
-    FOREIGN KEY (id_escuela) REFERENCES Escuelas(id_escuela) ON DELETE CASCADE
-);
-
--- Tabla estudiante
-CREATE table estudiante (
-	id_estudiante INT primary key AUTO_INCREMENT,
-    id_escuela INT,
-    nombre VARCHAR(255),
-    apellido VARCHAR(255),
+    primer_nombre VARCHAR (255),
+    segundo_nombre VARCHAR (255),
+    primer_apellido VARCHAR (255),
+    segundo_apellido VARCHAR (255),
+    edad INT,
     fecha_nacimiento DATE,
-    genero ENUM('Masculino', 'Femenino', 'Otro'),
+    sexo ENUM('Masculino', 'Femenino'),
     direccion VARCHAR(255),
     telefono VARCHAR(50),
     email VARCHAR(100),
     nombre_tutor VARCHAR(255),
     telefono_tutor VARCHAR(50),
-	FOREIGN KEY (id_escuela) REFERENCES Escuelas(id_escuela)
+	FOREIGN KEY (id_escuela) REFERENCES escuelas(id_escuela)
+)ENGINE=InnoDB;
+
+--Tabla Departamentos
+CREATE TABLE departamentos (
+    id_departamento INT PRIMARY KEY,
+    nombre_departamento VARCHAR (255)
+)ENGINE=InnoDB;
+
+--Tabla de Municipios
+CREATE TABLE municipios (
+    id_municipios INT PRIMARY KEY,
+    id_departamento INT,
+    nombre_municipio VARCHAR (255),
+    FOREIGN KEY (id_departamento) REFERENCES departamentos(id_departamento)
 );
-    
+
+--Tabla de Perfil de Salud
+CREATE TABLE salud_estudiante (
+    id_salud INT PRIMARY KEY,
+    id_estudiante INT,
+    peso FLOAT,
+    altura FLOAT,
+    imc FLOAT,
+    categoria_peso VARCHAR(255),
+    condicion_medica VARCHAR (255),
+    descripcion TEXT,
+    medicacion TEXT,
+    somatotipo VARCHAR (255)
+)ENGINE=InnoDB;
+
+--Tabla de Pruebas de Cualidades Físico-motrices
+CREATE TABLE puebras_fisicas (
+    id_pruebas INT PRIMARY KEY,
+    id_estudiante INT,
+    tipo_prueba VARCHAR (255),
+    resultado VARCHAR (255),
+    unidad_medida VARCHAR (255),
+    observaciones TEXT,
+    fecha_prueba DATE
+)ENGINE=InnoDB;
+
+-- -- Tabla MAESTROS
+-- CREATE TABLE maestros (
+--     id_maestros INT AUTO_INCREMENT PRIMARY KEY,
+--     id_escuela INT,
+--     nombre VARCHAR(255) NOT NULL,
+--     apellido VARCHAR(255) NOT NULL,
+--     email VARCHAR(100),
+--     telefono VARCHAR(50),
+--     perfil VARCHAR(100),
+--     FOREIGN KEY (id_escuela) REFERENCES Escuelas(id_escuela) ON DELETE CASCADE
+-- );
+
