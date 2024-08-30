dfb85f3ff163d2d322bd292115a315a6b002da2a
nanypoe <162480838+nanypoe@users.noreply.github.com>
2024-08-30 11:32:43 -0600

    Bug: no agrega Alumnos
    Se identificó un bug donde no se agregan los alumnos.

--- Models/estudianteModel.php
+++ Models/estudianteModel.php
@@ -1,36 +1,67 @@
 <?php
 
-class estudianteModel extends Model{
+class estudianteModel extends Model
+{
 
     function __construct()
     {
         parent::__construct();
     }
 
-    public function insertarEstudiante($id,$pNombre,$sNombre,$pApellido,$sApellido,$edad,$fecha,$sexo,$telefonoAlumno,$correo,$direccion,$nombreTutor,$telefonoTutor,$image){
-    $this->_db->prepare('insert into estudiantes(id_escuela,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,edad,fecha_nacimiento,sexo, direccion,telefono,email,nombre_tutor,telefono_tutor,imagen)values(:id,:pNombre,:sNombre,:pApellido,:sApellido,:edad,:fecha,:sexo,:telefonoAlumno,:correo,:direccion,:nombreTutor,:telefonoTutor,:image)')->execute(array('id'=>$id,
-    'pNombre'=>$pNombre,'sNombre'=>$sNombre,'pApellido'=>$pApellido,'sApellido'=>$sApellido
-    ,'edad'=>$edad,'fecha'=>$fecha,'sexo'=>$sexo,'telefonoAlumno'=>$telefonoAlumno,'correo'=>$correo,'direccion'=>$direccion,'nombreTutor'=>$nombreTutor,'telefonoTutor'=>$telefonoTutor,'image'=>$image));
+    public function insertarEstudiante($id, $pNombre, $sNombre, $pApellido, $sApellido, $edad, $fecha, $sexo, $telefonoAlumno, $correo, $direccion, $nombreTutor, $telefonoTutor, $imagen)
+    {
+        $this->_db->prepare('INSERT INTO estudiantes (id_escuela, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, edad, fecha_nacimiento, sexo, direccion, telefono, email, nombre_tutor, telefono_tutor, imagen) VALUES (:id, :pNombre, :sNombre, :pApellido, :sApellido, :edad, :fecha, :sexo, :telefonoAlumno, :correo, :direccion, :nombreTutor,:telefonoTutor, :imagen);')->execute(array(
+            'id' => $id,
+            'pNombre' => $pNombre,
+            'sNombre' => $sNombre,
+            'pApellido' => $pApellido,
+            'sApellido' => $sApellido,
+            'fecha' => $edad,
+            'edad' => $fecha,
+            'sexo' => $sexo,
+            'telefonoAlumno' => $telefonoAlumno,
+            'correo' => $correo,
+            'direccion' => $direccion,
+            'nombreTutor' => $nombreTutor,
+            'telefonoTutor' => $telefonoTutor,
+            'imagen' => $imagen
+        ));
     }
 
-    public function insertarEstSinImagen($id,$pNombre,$sNombre,$pApellido,$sApellido,$fecha,$edad,$sexo,$telefonoAlumno,$correo,$direccion,$nombreTutor,$telefonoTutor){
-        $this->_db->prepare('insert into estudiantes(id_escuela,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,edad,direccion,telefono,email,nombre_tutor,telefono_tutor,imagen)values(:id,:pNombre,:sNombre,:pApellido,:sApellido,:fecha,:edad,:sexo,:telefonoAlumno,:correo,:direccion,:nombreTutor,:telefonoTutor,:image)')->execute(array('id'=>$id,
-        'pNombre'=>$pNombre,'sNombre'=>$sNombre,'pApellido'=>$pApellido,'sApellido'=>$sApellido
-        ,'fecha'=>$fecha,'edad'=>$edad,'sexo'=>$sexo,'telefonoAlumno'=>$telefonoAlumno,'correo'=>$correo,'direccion'=>$direccion,'nombreTutor'=>$nombreTutor,'telefonoTutor'=>$telefonoTutor));
+    public function insertarEstSinImagen($id, $pNombre, $sNombre, $pApellido, $sApellido, $fecha, $edad, $sexo, $telefonoAlumno, $correo, $direccion, $nombreTutor, $telefonoTutor)
+    {
+        $this->_db->prepare('INSERT INTO estudiantes (id_escuela, primer_nombre,segundo_nombre, primer_apellido, segundo_apellido, edad, fecha, sexo,  direccion, telefono, email, nombre_tutor, telefono_tutor) VALUES (:id, :pNombre, :sNombre, :pApellido, :sApellido, :edad, :fecha, :sexo, :telefonoAlumno, :correo, :direccion, :nombreTutor, :telefonoTutor);')->execute(array(
+            'id' => $id,
+            'pNombre' => $pNombre,
+            'sNombre' => $sNombre,
+            'pApellido' => $pApellido,
+            'sApellido' => $sApellido,
+            'fecha' => $edad,
+            'edad' => $fecha,
+            'sexo' => $sexo,
+            'telefonoAlumno' => $telefonoAlumno,
+            'correo' => $correo,
+            'direccion' => $direccion,
+            'nombreTutor' => $nombreTutor,
+            'telefonoTutor' => $telefonoTutor
+        ));
     }
 
-    public function obtenerAlumno(){
+    public function obtenerAlumno()
+    {
         return $this->_db->query("select esc.nombre as nombre_escuela,est.id_estudiante,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,est.fecha_nacimiento,est.sexo,est.direccion,est.telefono,
         est.email,est.nombre_tutor,est.telefono_tutor from estudiantes as est inner join escuelas as
         esc on esc.id_escuela=est.id_escuela;")->fetchAll();
     }
 
-    public function obtenerEscuela(){
+    public function obtenerEscuela()
+    {
         return $this->_db->query("select *from escuelas")->fetchAll();
     }
 
-    public function borrarAlum($id){
-        $this->_db->prepare('delete from alumno where id=:id')->execute(array('id'=>$id));
+    public function borrarAlum($id)
+    {
+        $this->_db->prepare('delete from alumno where id=:id')->execute(array('id' => $id));
     }
 
 
--- Views/plantilla/js/funciones.js
+++ Views/plantilla/js/funciones.js
@@ -1,6 +1,6 @@
 /* Inicailizando el doom */
 
-$(function(){
+$(function () {
 
     /* Ininicializacion de dataTable */
     $('#table').DataTable({
@@ -8,36 +8,36 @@ $(function(){
         buttons: [
             'copy', 'excel', 'pdf', 'print'
         ],
-        language:{
-            "sSearch":"Buscar",
-            "info":"Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "zeroRecords":"No se encuentraron coincidencias",
-            "infoEmpty":"Mostrando 0 a 0 de 0 registros",
-            "infoFiltered":"(filtrado de un total de _MAX_ registros)",
+        language: {
+            "sSearch": "Buscar",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "zeroRecords": "No se encuentraron coincidencias",
+            "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
         },
-        responsive:true,
+        responsive: true,
         columnDefs: [
-            {"className": "dt-center", "targets": "_all"}
-          ]
+            { "className": "dt-center", "targets": "_all" }
+        ]
     });
 
     /*------- Funciones para Gerentes------------ */
 
     /* Funcion para enviar los datos de los gerentes*/
 
-    $('#formAgregarEscuela').submit(function(e){
+    $('#formAgregarEscuela').submit(function (e) {
         e.preventDefault();
-        let nombreEscuela=$('#nombreEscuela').val();
-        let direccionEscuela=$('#direccionEscuela').val();
-        let telefonoEscuela=$('#telefonoEscuela').val();
-        let latitudEscuela=$('#latitudEscuela').val();
-        let longitudEscuela=$('#longitudEscuela').val();
-        
+        let nombreEscuela = $('#nombreEscuela').val();
+        let direccionEscuela = $('#direccionEscuela').val();
+        let telefonoEscuela = $('#telefonoEscuela').val();
+        let latitudEscuela = $('#latitudEscuela').val();
+        let longitudEscuela = $('#longitudEscuela').val();
+
         $.ajax({
-            url:'escuela/agregarEscuela/',
-            type:'post',
-            data:{'nombreEscuela':nombreEscuela,'direccionEscuela':direccionEscuela,'telefonoEscuela':telefonoEscuela,'latitudEscuela':latitudEscuela,'longitudEscuela':longitudEscuela},
-            success:function(respuesta){
+            url: 'escuela/agregarEscuela/',
+            type: 'post',
+            data: { 'nombreEscuela': nombreEscuela, 'direccionEscuela': direccionEscuela, 'telefonoEscuela': telefonoEscuela, 'latitudEscuela': latitudEscuela, 'longitudEscuela': longitudEscuela },
+            success: function (respuesta) {
                 $('#modalAgregarEscuela').modal('hide');
                 $('#formAgregarEscuela')[0].reset();
                 $('#table').DataTable().destroy();
@@ -47,14 +47,14 @@ $(function(){
                     buttons: [
                         'copy', 'excel', 'pdf', 'print'
                     ],
-                    language:{
-                        "sSearch":"Buscar",
-                        "info":"Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                        "zeroRecords":"No se encuentraron coincidencias",
-                        "infoEmpty":"Mostrando 0 a 0 de 0 registros",
-                        "infoFiltered":"(filtrado de un total de _MAX_ registros)",
+                    language: {
+                        "sSearch": "Buscar",
+                        "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                        "zeroRecords": "No se encuentraron coincidencias",
+                        "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+                        "infoFiltered": "(filtrado de un total de _MAX_ registros)",
                     },
-                    responsive:true 
+                    responsive: true
                 });
                 Swal.fire({
                     title: "Agregado!",
@@ -69,93 +69,91 @@ $(function(){
 
     /* Funcion para agregar gym */
 
-    $("#formAgregarGym").on("submit",function(e){
-    var extension=$("#imagen").val().split('.').pop().toLowerCase();;
-    console.log(extension);
-    if(extension != '')
-      {
-       if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)
-       {
-        alert("Invalid Image File");
-        $('#imagen').val('');
-        return false;
-       }
-      } 
+    $("#formAgregarGym").on("submit", function (e) {
+        var extension = $("#imagen").val().split('.').pop().toLowerCase();;
+        console.log(extension);
+        if (extension != '') {
+            if (jQuery.inArray(extension, ['gif', 'png', 'jpg', 'jpeg']) == -1) {
+                alert("Invalid Image File");
+                $('#imagen').val('');
+                return false;
+            }
+        }
         e.preventDefault();
         $.ajax({
-            url:'gimnasio/agregarGym/',
-            type:'post',
-            data:new FormData(this),
-            contentType:false,
-            processData:false,
-        success:function(respuesta){
-      
-            $('#modalAgregarGym').modal('hide');
-            $("#table").DataTable().destroy();
-            $("#table tbody").html(respuesta);
-            $('#table').DataTable({
-                dom: 'Bfrtip',
-                buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
-                ],
-                responsive: true,rowReorder: {
-                    selector: 'td:nth-child(2)'
-                },
-                "language":{
-                    "sSearch":"Buscar",
-                    "lengthMenu":"Mostrar MENU registros",
-                    "infoEmpty":"Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "zeroRecords":"No se encuentran resultados",
-                    "info":"Mostrando registros del START al END de un TOTAL de registros",
-                    "oPaginate":{
-                        "sNext":"Siguiente",
-                        "sPrevious":"Anterior"
-                    }        
-                }
-            
-            });  
-    
-            $('#formAgregarGym')[0].reset();
-    
-            Swal.fire(
-                'Se registro el evento con exito',
-                'en el sistema',
-                'success'
-            )
-    
-    
-        }
-        ,error:function(){
-            console.log('Error');
-        }
-    
-    
-    
-    }); 
-    
-    
-    
+            url: 'gimnasio/agregarGym/',
+            type: 'post',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (respuesta) {
+
+                $('#modalAgregarGym').modal('hide');
+                $("#table").DataTable().destroy();
+                $("#table tbody").html(respuesta);
+                $('#table').DataTable({
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'copy', 'csv', 'excel', 'pdf', 'print'
+                    ],
+                    responsive: true, rowReorder: {
+                        selector: 'td:nth-child(2)'
+                    },
+                    "language": {
+                        "sSearch": "Buscar",
+                        "lengthMenu": "Mostrar MENU registros",
+                        "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                        "zeroRecords": "No se encuentran resultados",
+                        "info": "Mostrando registros del START al END de un TOTAL de registros",
+                        "oPaginate": {
+                            "sNext": "Siguiente",
+                            "sPrevious": "Anterior"
+                        }
+                    }
+
+                });
+
+                $('#formAgregarGym')[0].reset();
+
+                Swal.fire(
+                    'Se registro el evento con exito',
+                    'en el sistema',
+                    'success'
+                )
+
+
+            }
+            , error: function () {
+                console.log('Error');
+            }
+
+
+
+        });
+
+
+
     });
 
 
-     /*------- Funciones para Maestros------------ */
+    /*------- Funciones para Maestros------------ */
 
     /* Funcion para enviar los datos de los Maestros*/
 
-    $('#formAgregarMaestros').submit(function(e){
+    $('#formAgregarMaestros').submit(function (e) {
         e.preventDefault();
-        let idEscuela=$('#id').val();
-        let nombre=$('#nombre').val();
-        let apellido=$('#apellido').val();
-        let correo=$('#correo').val();
-        let telefono=$('#telefono').val();
-        let perfil=$('#perfil').val();
-        
+        let idEscuela = $('#id').val();
+        let nombre = $('#nombre').val();
+        let apellido = $('#apellido').val();
+        let correo = $('#correo').val();
+        let telefono = $('#telefono').val();
+        let perfil = $('#perfil').val();
+
         $.ajax({
-            url:'maestros/agregarMaestros/',
-            type:'post',
-            data:{'id':idEscuela,'nombre':nombre,'apellido':apellido,'correo':correo,'telefono':telefono,'perfil':perfil},
-            success:function(respuesta){
+            url: 'maestros/agregarMaestros/',
+            type: 'post',
+            data: { 'id': idEscuela, 'nombre': nombre, 'apellido': apellido, 'correo': correo, 'telefono': telefono, 'perfil': perfil },
+            success: function (respuesta) {
                 $('#modalAgregarMaestro').modal('hide');
                 $('#formAgregarMaestros')[0].reset();
                 $('#table').DataTable().destroy();
@@ -165,14 +163,14 @@ $(function(){
                     buttons: [
                         'copy', 'excel', 'pdf', 'print'
                     ],
-                    language:{
-                        "sSearch":"Buscar",
-                        "info":"Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                        "zeroRecords":"No se encuentraron coincidencias",
-                        "infoEmpty":"Mostrando 0 a 0 de 0 registros",
-                        "infoFiltered":"(filtrado de un total de _MAX_ registros)",
+                    language: {
+                        "sSearch": "Buscar",
+                        "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                        "zeroRecords": "No se encuentraron coincidencias",
+                        "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+                        "infoFiltered": "(filtrado de un total de _MAX_ registros)",
                     },
-                    responsive:true 
+                    responsive: true
                 });
                 Swal.fire({
                     title: "Agregado!",
@@ -184,34 +182,40 @@ $(function(){
     });
 
 
-     /*------- Funciones para Alumnos------------ */
+    /*------- Funciones para Alumnos------------ */
 
     /* Funcion para enviar los datos de los Alumnos*/
 
-    $('#formAgregarAlumno').submit(function(e){
+    $('#formAgregarAlumno').submit(function (e) {
         e.preventDefault();
-        let extension=$('#imagenEstudiante').val().split('.').pop().toLowerCase();
+        let extension = $('#imagenEstudiante').val().split('.').pop().toLowerCase();
         e.preventDefault();
-        if(extension != ''){
-            if(jQuery.inArray(extension,['png','jpg','jpeg'])==-1 ){
+        if (extension != '') {
+            if (jQuery.inArray(extension, ['png', 'jpg', 'jpeg']) == -1) {
                 $('#imagenEstudiante').val('');
                 alert("Formato de imagen incorrecto");
                 return false;
             }
         }
         $.ajax({
-            url:'estudiante/agregarAlumno/',
-            type:'post',
-            data:new FormData(this),
-            contentType:false,
-            processData:false,
-            success:function(respuesta){
+            url: 'estudiante/agregarAlumno/',
+            type: 'post',
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log("success");
+                console.log(data);
                 $('#formAgregarAlumno')[0].reset();
                 Swal.fire({
                     title: "Agregado!",
                     text: "El registro a sido Agregado de forma correcta.",
                     icon: "success"
                 });
+            },
+            error: function (data) {
+                console.log("error");
+                console.log(data);
             }
         });
     });
