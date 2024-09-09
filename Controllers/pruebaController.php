fb59731adb41606d891e102aac5a0e23a8ba0658
EngelAfk <160043373+EngelAfk@users.noreply.github.com>
2024-09-09 12:05:40 -0600

    Se inicia la funcionalidad Agregar en pruebas físicas
    Se agrega la funcionalidad para Ingresar nuevas pruebas en la tabla Pruebas Físicas

--- Controllers/pruebaController.php
+++ Controllers/pruebaController.php
@@ -15,16 +15,17 @@
                 $datos=json_encode($fila[$i]);
                 $tabla.='
                 <tr>
+                    <td>'.$fila[$i]['primer_nombre'].' ' .$fila[$i]['segundo_nombre'].' '.$fila[$i]['primer_apellido'].' '.$fila[$i]['segundo_apellido'].'</td>
                     <td>'.$fila[$i]['id_prueba'].'</td>
                     <td>'.$fila[$i]['tipo'].'</td>
-                    <td>'.$fila[$i]['resultado'].'</td>
                     <td>'.$fila[$i]['unidad'].'</td>
+                    <td>'.$fila[$i]['resultado'].'</td>
                     <td>'.$fila[$i]['observacion'].'</td>
                     <td>'.$fila[$i]['fecha'].'</td>
+                    
                     <td>
-                    <button data-puebras_fisicas=\''.$datos.'\'  data-bs-toggle="modal" data-bs-target="#modalEditarEscuela" type="button" style="color:white;font-weight:bold" class="btn btn-warning btnEditarEscuela"><i class="fa-solid fa-rotate-right"></i> Actualizar</button>  
-        <button data-id='.$fila[$i]['id_prueba'].' type="button" style="color:white;font-weight:bold" class="btn btn-danger BtnBorrarEscuela"><i class="fa-solid fa-trash"></i> Borrar</button> 
-
+                    <button data-puebras=\''.$datos.'\'  data-bs-toggle="modal" data-bs-target="#modalEditarEscuela" type="button" style="color:white;font-weight:bold" class="btn btn-warning btnEditarEscuela"><i class="fa-solid fa-rotate-right"></i> Actualizar</button>  
+                    <button data-id='.$fila[$i]['id_prueba'].' type="button" style="color:white;font-weight:bold" class="btn btn-danger BtnBorrarEscuela"><i class="fa-solid fa-trash"></i> Borrar</button> 
                     </td>
                 </tr>
                 ';
@@ -35,30 +36,26 @@
 
         public function index()
         {
+             /* mandando a la vista los datos de los estudiantes */
+             $fila=$this->_prue->obtenerEstudiante();
+             $datos='<option value="0"> Seleccione un estudiante</option>';
+             for($i=0;$i<count($fila);$i++){
+                 $datos.= '<option value="'.$fila[$i]['id_estudiante'].'">'.$fila[$i]['primer_nombre'].' '.$fila[$i]['segundo_nombre'].' '.$fila[$i]['primer_apellido'].' '.$fila[$i]['segundo_apellido'].'</option>';
+             }
+             $this->_view->estudiantes=$datos;
             $this->_view->tabla=$this->verPrueba(); 
             $this->_view->renderizar('prueba');
             
         }
 
-        
-        public function agregarEstudiante(){
-            /*mandando id de estudiante */
-            $fila = $this->_prue->obtenerPrueba();
-            $datos = '<option value="0"> Seleccione el id del estudiante </option>';
-             for ($i = 0; $i < count($fila); $i++) {
-            $datos .= '<option value="' . $fila[$i]['id_estudiante'] . '">' . $fila[$i]['nombre'] . '</option>';
-        }
-        
-        $this->_view->estudiante = $datos;
-        $this->_view->renderizar('agregar');
-        }
-
         public function agregarPrueba(){
-            $this->_prue->agregarPru($this->getTexto('tipoPrueba'),$this->getTexto('resultadoPrueba'),$this->getTexto('unidadPrueba'),$this->getTexto('observacionPrueba'),$this->getTexto('axo'));
+            $this->_prue->agregarPru($this->getTexto('idE'),$this->getTexto('tipoPrueba'),$this->getTexto('resultadoPrueba'),$this->getTexto('unidadPrueba'),$this->getTexto('observacionPrueba'),$this->getTexto('axo'));
 
             echo $this->verPrueba();
 
         }
+        
+        
 
         public function editarEscuela(){
             $this->_escue->editarEscue($this->getTexto('id_escuela'),$this->getTexto('nombre'),$this->getTexto('direccion'),$this->getTexto('telefono'),$this->getTexto('longitud'),$this->getTexto('latitud'));
