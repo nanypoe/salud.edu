b54c54317f8373668d20e6d842bd7fecb901a614
Renaldy Sánchez <162480838+nanypoe@users.noreply.github.com>
2024-09-26 06:13:10 -0600

    Se agregan vistas y funciones
    - Se crea vista para el Index
    
    - Ahora se puede agregar en la tabla PRUEBAS FÍSICAS si un usuario con rol de "docente" inicia sesión.
    
    - Ahora se puede visualizar el perfil del estudiante, si se accede desde un usuario con rol "estudiante"

--- Models/pruebaModel.php
+++ Models/pruebaModel.php
@@ -2,41 +2,66 @@
 
 class pruebaModel extends Model
 {
-
     function __construct()
     {
         parent::__construct();
     }
 
-/*Función para OBTENER datos de PRUEBAS FÍSICAS para la Vista*/
-public function obtenerDatosPruebas()
-{
-    return $this->_db->query("SELECT * FROM pruebas_fisicas INNER JOIN estudiantes ON pruebas_fisicas.id_estudiante=estudiantes.id_estudiante")->fetchAll();
+    //Obtener ESTUDIANTES
+    public function obtenerDatosDocente($usuario)
+    {
+        return $this->_db->query("SELECT  * FROM docentes WHERE usuario = '$usuario'")->fetchAll();
+    }
+    public function sesionPrueba()
+    {
+        return $this->_db->query("SELECT  * FROM estudiantes")->fetchAll();
+    }
+
+
+    //Obtener GRUPOS
+    public function obtenerGrupos($id)
+    {
+        return $this->_db->query("SELECT * FROM grupos INNER JOIN docentes ON grupos.docente_id=docentes.id_docente WHERE docente_id='$id';")->fetchAll();
+    }
+
+    public function getEstudiante($id)
+    {
+        $estudiante_id_query = "SELECT id_estudiante FROM matricula WHERE id_grupo='$id'";
+        $estudiante_ids = $this->_db->query($estudiante_id_query)->fetchAll();
+
+        $has_salud_estudiante_records = false;
+        foreach ($estudiante_ids as $estudiante_id) {
+            $salud_estudiante_query = "SELECT COUNT(*) as count FROM salud_estudiante WHERE id_estudiante={$estudiante_id['id_estudiante']}";
+            $salud_estudiante_count = $this->_db->query($salud_estudiante_query)->fetch();
+            if ($salud_estudiante_count['count'] > 0) {
+                $has_salud_estudiante_records = true;
+                break;
+            }
+        }
+
+        if (!$has_salud_estudiante_records) {
+            // Execute the first query if no records are found in salud_estudiante
+            return $this->_db->query("SELECT * FROM grupos INNER JOIN matricula ON grupos.id_grupo=matricula.id_grupo INNER JOIN  estudiantes ON matricula.id_estudiante=estudiantes.id_estudiante WHERE grupos.id_grupo='$id'")->fetchAll();
+        } else {
+            // Execute the second query if records are found in salud_estudiante
+            return $this->_db->query("SELECT * FROM grupos INNER JOIN matricula ON grupos.id_grupo=matricula.id_grupo INNER JOIN  estudiantes ON matricula.id_estudiante=estudiantes.id_estudiante INNER JOIN salud_estudiante ON salud_estudiante.id_estudiante=estudiantes.id_estudiante WHERE grupos.id_grupo='$id'")->fetchAll();
+        }
+    }
+
+    public function agregarPrueba($idEstudiante, $idGrupo, $fecha, $prueba, $resultado, $medida, $observacion)
+    {
+        $this->_db->prepare("INSERT INTO pruebas_fisicas (id_estudiante, fecha_prueba, tipo_prueba, resultado, unidad_medida, observaciones) VALUES (:idEstudiante, :fecha, :prueba, :resultado, :medida, :observacion)")->execute(
+            array(
+                "idEstudiante" => $idEstudiante,
+                "fecha" => $fecha,
+                "prueba" => $prueba,
+                "resultado" => $resultado,
+                "medida" => $medida,
+                "observacion" => $observacion
+            )
+        );
+        echo $idGrupo;
+    }
 }
 
-    public function agregarPrueba($idE, $tipo, $resultado, $unidad, $observacion, $fecha)
-    {
-        $this->_db->prepare("INSERT INTO puebras_fisicas (id_estudiante,tipo, resultado, unidad, observacion, fecha) VALUES (:idE,:tipo, :resultado, :unidad, :observacion, :fecha)")->execute(array('idE' => $idE,'tipo' => $tipo, 'resultado' => $resultado, 'unidad' => $unidad, 'observacion' => $observacion, 'fecha' => $fecha));
-    }
 
-    public function editarEscuela($nombre, $direccion, $telefono, $longitud, $latitud, $id_escuela)
-    {
-        $this->_db->prepare("UPDATE escuelas set nombre=:nombre, direccion=:direccion, telefono=:telefono, longitud=:longitud, latitud=:latitud where id_escuela=:idEscuela")->execute(array('nombre' => $nombre, 'direccion' => $direccion, 'telefono' => $telefono, 'longitud' => $longitud, 'latitud' => $latitud, 'id_escuela' => $id_escuela));
-    }
-
-    public function obtenerPrueba()
-    {
-        return $this->_db->query("SELECT * FROM puebras_fisicas")->fetchAll();
-    }
-
-    public function obtenerEstudiante()
-    {
-        return $this->_db->query("SELECT * FROM  estudiantes")->fetchAll();
-    }
-
-    public function borrarEscuela($id_escuela)
-    {
-        $this->_db->prepare('DELETE FROM Escuelas WHERE id_escuela=:id_escuela')->execute(array('id_escuela' => $id_escuela));
-    }
-}
-?>
